85670d7c5f85f40042af590db00f3fd5b73eaab7
sod-lol <ddssee5511@gmail.com>
2021-05-27 11:30:20 -0400

    Baton test bug fixed, Baton fairness test added

--- tests/synchronization/Baton.cpp
+++ tests/synchronization/Baton.cpp
@@ -79,8 +79,11 @@ std::string gen_random(const int len) {
 TEST(BatonBasicOpreation, CheckThreadSaftyReaderWriter)
 {
     thunder::synchronization::Baton baton;
+    thunder::synchronization::Baton resBaton;
 
     std::vector<std::thread> threads;
+    std::vector<std::thread::id> writerThreadsID;
+    std::vector<std::thread::id> readerThreadsID;
 
     std::vector<std::tuple<int, std::thread::id, std::string, std::thread::id>> result;
     std::vector<std::tuple<int, std::thread::id, std::string>> queue;
@@ -89,18 +92,19 @@ TEST(BatonBasicOpreation, CheckThreadSaftyReaderWriter)
 
     int count_per_thread = 2000;
 
-    std::cout << 1 << std::endl;
-
     for (size_t i = 0; i < 8; i++) {
         
         std::thread t1([&](){
+            baton.write_access_start();
+            writerThreadsID.push_back(std::this_thread::get_id());
+            baton.write_access_end();
 
             for (size_t j = 0; j < count_per_thread; j++)
             {
-                std::cout << 2 << std::endl;
                 baton.write_access_start();
                 queue.push_back(std::tuple<int, std::thread::id, std::string>(j, std::this_thread::get_id(), gen_random(j % 30)));
                 std::this_thread::sleep_for(std::chrono::microseconds(40));
+                total++;
                 baton.write_access_end();
             }
             
@@ -112,33 +116,33 @@ TEST(BatonBasicOpreation, CheckThreadSaftyReaderWriter)
         
         std::thread t1([&](){
 
+            baton.write_access_start();
+            readerThreadsID.push_back(std::this_thread::get_id());
+            baton.write_access_end();
+
             for (size_t j = 0; j < count_per_thread; j++)
             {
                 if (queue.empty()) {
                     j--;
-                    std::cout << 3 << std::endl;
                     std::this_thread::sleep_for(std::chrono::microseconds(20));
                     continue;
                 }
 
-                std::cout << 4 << std::endl;
-
                 baton.read_access_start();
-                if (queue.empty())
-                {
+                resBaton.write_access_start();
+                if (queue.empty()) {
                     j--;
-                    std::cout << 5 << std::endl;
                     std::this_thread::sleep_for(std::chrono::microseconds(20));
+                    resBaton.write_access_end();
                     baton.read_access_end();
                     continue;
                 }
                 auto s = queue.back();
                 queue.pop_back();
-                std::cout << 6 << std::endl;
                 std::sort(std::get<2>(s).begin(), std::get<2>(s).end());
-                std::cout << 7 << std::endl;
-                result.push_back(std::tuple<int, std::thread::id, std::string, std::thread::id>(std::get<0>(s), std::get<1>(s), std::get<2>(s), std::this_thread::get_id()));
-                std::cout << 8 << std::endl;
+                result.push_back(std::tuple<int, std::thread::id, std::string, std::thread::id>(j, std::get<1>(s), std::get<2>(s), std::this_thread::get_id()));
+                resBaton.write_access_end();
+                std::this_thread::sleep_for(std::chrono::microseconds(20));
                 baton.read_access_end();
             }
             
@@ -150,7 +154,155 @@ TEST(BatonBasicOpreation, CheckThreadSaftyReaderWriter)
         threads.at(i).join();
     }
 
-    EXPECT_EQ(total, count_per_thread*16);
-    
-    
+    std::map<std::thread::id, int> resultCounter;
+    std::map<std::thread::id, int> processedCounter;
+
+
+
+    EXPECT_EQ(total, count_per_thread*8);
+    EXPECT_EQ(result.size(), count_per_thread*8);
+
+    for (auto& wirterThreads: writerThreadsID)
+    {
+        resultCounter[wirterThreads] = 0;
+    }
+
+    for (auto& readerThreads: readerThreadsID)
+    {
+        processedCounter[readerThreads] = 0;
+    }
+
+
+    for (auto& res: result)
+    {
+        resultCounter[std::get<1>(res)] = resultCounter[std::get<1>(res)] + 1;
+        processedCounter[std::get<3>(res)] = processedCounter[std::get<3>(res)] + 1;
+    }
+
+    for (const auto& [key, value] : resultCounter) {
+        EXPECT_EQ(value, count_per_thread);
+    }
+
+    for (const auto& [key, value] : processedCounter) {
+        EXPECT_EQ(value, count_per_thread);
+    }
+}
+
+
+
+TEST(BatonBasicOpreation, CheckReaderFairness)
+{
+    thunder::synchronization::Baton baton;
+    thunder::synchronization::Baton resBaton;
+
+    std::vector<std::thread> threads;
+    std::vector<std::thread::id> writerThreadsID;
+    std::vector<std::thread::id> readerThreadsID;
+
+    std::vector<std::tuple<int, std::thread::id, std::string, std::thread::id>> result;
+    std::vector<std::tuple<int, std::thread::id, std::string>> queue;
+
+    int total = 0;
+
+    int finishedThread = 0;
+
+    int count_per_thread = 2000;
+
+    for (size_t i = 0; i < 8; i++) {
+        
+        std::thread t1([&](){
+            baton.write_access_start();
+            writerThreadsID.push_back(std::this_thread::get_id());
+            baton.write_access_end();
+
+            for (size_t j = 0; j < count_per_thread; j++)
+            {
+                baton.write_access_start();
+                queue.push_back(std::tuple<int, std::thread::id, std::string>(j, std::this_thread::get_id(), gen_random(j % 30)));
+                std::this_thread::sleep_for(std::chrono::microseconds(40));
+                total++;
+                baton.write_access_end();
+            }
+            baton.write_access_start();
+            finishedThread++;
+            baton.write_access_end();
+        });
+        threads.push_back(std::move(t1));
+    }
+
+    for (size_t i = 0; i < 8; i++) {
+        
+        std::thread t1([&](){
+
+            baton.write_access_start();
+            readerThreadsID.push_back(std::this_thread::get_id());
+            baton.write_access_end();
+
+            while (finishedThread != 8)
+            {
+                if (queue.empty()) {
+                    std::this_thread::sleep_for(std::chrono::microseconds(20));
+                    continue;
+                }
+
+                baton.read_access_start();
+                resBaton.write_access_start();
+                if (queue.empty()) {
+                    std::this_thread::sleep_for(std::chrono::microseconds(20));
+                    resBaton.write_access_end();
+                    baton.read_access_end();
+                    continue;
+                }
+                auto s = queue.back();
+                queue.pop_back();
+                std::sort(std::get<2>(s).begin(), std::get<2>(s).end());
+                result.push_back(std::tuple<int, std::thread::id, std::string, std::thread::id>(std::get<0>(s), std::get<1>(s), std::get<2>(s), std::this_thread::get_id()));
+                resBaton.write_access_end();
+                std::this_thread::sleep_for(std::chrono::microseconds(20));
+                baton.read_access_end();
+            }
+            
+        });
+        threads.push_back(std::move(t1));
+    }
+
+    for (int i = 0; i < 16; i++) {
+        threads.at(i).join();
+    }
+
+    std::map<std::thread::id, int> resultCounter;
+    std::map<std::thread::id, int> processedCounter;
+
+
+
+    EXPECT_EQ(total, count_per_thread*8);
+    EXPECT_EQ(result.size(), count_per_thread*8);
+
+    for (auto& wirterThreads: writerThreadsID)
+    {
+        resultCounter[wirterThreads] = 0;
+    }
+
+    for (auto& readerThreads: readerThreadsID)
+    {
+        processedCounter[readerThreads] = 0;
+    }
+
+
+    for (auto& res: result)
+    {
+        resultCounter[std::get<1>(res)] = resultCounter[std::get<1>(res)] + 1;
+        processedCounter[std::get<3>(res)] = processedCounter[std::get<3>(res)] + 1;
+    }
+
+    for (const auto& [key, value] : resultCounter) {
+        EXPECT_EQ(value, count_per_thread);
+    }
+
+    int totalNumberOfProcessedItem = count_per_thread * 8;
+
+    for (const auto& [key, value] : processedCounter) {
+        std::cout << "Thread with id: " << key << " processed items with percent of: " \
+         << (float)value * 100 / (float)totalNumberOfProcessedItem << "%" << std::endl;
+    }
 }
