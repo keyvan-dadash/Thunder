a18af62e463ee426f3fbc8d8645f72b94acffa57
keyvan1379 <ddssee5511@gmail.com>
2021-08-05 00:19:54 -0400

    basic ThreadPool api added, cmake file changed to support include from other directory of project

---
+++ src/thunder/pools/ThreadPool.cpp
@@ -0,0 +1,33 @@
+
+
+
+#include <thread>
+#include <memory>
+#include <future>
+
+
+#include "ThreadPool.hpp"
+
+
+
+namespace thunder {
+
+  namespace pools {
+
+
+
+
+    void ThreadPool::handleTasks()
+    {
+
+      
+    }
+
+
+
+
+
+
+
+  };
+}
--- src/thunder/pools/ThreadPool.hpp
+++ src/thunder/pools/ThreadPool.hpp
@@ -2,14 +2,69 @@
 #pragma once
 
 
+#include <memory>
+#include <thread>
+#include <future>
+
+#include "thunder/datastructures/AtomicQueue.hpp"
+
+#include "Task.hpp"
+
 namespace thunder {
 
 
   namespace pools {
-    
+
+    struct ThreadPoolOptions
+    {
+      size_t numberOfThreads;
+    };
 
 
-  }
+    class ThreadPool
+    {
+
+
+    public:
+
+      ThreadPool(ThreadPoolOptions options) : options_(options)
+      {
+
+      }
+
+      ThreadPool(ThreadPool&& other) = delete;
+      ThreadPool& operator=(ThreadPool&& rhs) = delete;
+
+      
+      ThreadPool(const ThreadPool& other) = delete;
+      ThreadPool& operator=(const ThreadPool& rhs) = delete;
+
+      ~ThreadPool()
+      {
+
+      }
+
+
+      template<typename FuncType>
+      std::future<
+        typename std::result_of_t<FuncType()>
+      > submit(FuncType func);
+
+
+      void handleTasks();
+
+    private:
+
+      ThreadPoolOptions options_;
+
+      thunder::datastructures::AtomicQueue<Task> tasks_;
+
+      bool isRunning_;
+
+
+    };
+
+  };
 
 
 }
