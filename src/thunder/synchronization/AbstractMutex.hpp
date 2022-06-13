9db7c22c375ef1a9af4969970dc3fc3386078fe7
sod-lol <ddssee5511@gmail.com>
2022-06-13 07:07:54 -0400

    Add abstract mutex

---
+++ src/thunder/synchronization/AbstractMutex.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace thunder {
+
+  namespace synchronization {
+
+    class AbstractMutex
+    {
+      public:
+
+        AbstractMutex() = default;
+
+        virtual void lock() = 0;
+
+        virtual void unlock() = 0;
+
+        virtual void isLocked() = 0;
+    };
+  };
+}
+
