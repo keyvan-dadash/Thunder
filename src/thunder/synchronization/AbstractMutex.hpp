a07b1e75ecbee29a7a0c3717b46f051060dc0873
sod-lol <ddssee5511@gmail.com>
2022-11-16 02:21:37 -0500

    Add doc for AbstractMutex

--- src/thunder/synchronization/AbstractMutex.hpp
+++ src/thunder/synchronization/AbstractMutex.hpp
@@ -1,18 +1,39 @@
+/**
+ * @file AbstractMutex.hpp
+ * Contains AbstractMutex class which all other implementation of
+ * mutex iherith from.
+ */
 #pragma once
 
 namespace thunder {
 
 namespace synchronization {
 
+/**
+ * \brief Abstract class for mutex.
+ *
+ * AbstractMutex is a abstract class which specify what api's
+ * should mutex expose.
+ */
 class AbstractMutex {
  public:
   AbstractMutex() = default;
 
+  /**
+   * \brief lock the mutex.
+   */
   virtual void lock() = 0;
 
+  /**
+   * \brief unlock the mutex.
+   */
   virtual void unlock() = 0;
 
-  virtual void isLocked() = 0;
+  /**
+   * \brief check whatever the muted is Locked or not.
+   * \return boolean which tell is the mutex locked or not.
+   */
+  virtual bool isLocked() = 0;
 };
 };  // namespace synchronization
 }  // namespace thunder
