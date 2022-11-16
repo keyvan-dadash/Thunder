5869e08378e39ba97e01f2933eb7cb55d0bce87e
sod-lol <ddssee5511@gmail.com>
2022-11-16 03:02:47 -0500

    Add doc for SpinLock

--- src/thunder/synchronization/SpinLock.hpp
+++ src/thunder/synchronization/SpinLock.hpp
@@ -1,3 +1,6 @@
+/**
+ * @file SpinLock.hpp
+ */
 #pragma once
 
 #include <atomic>
@@ -21,10 +24,16 @@ constexpr std::size_t hardware_destructive_interference_size =
     2 * sizeof(std::max_align_t);
 #endif
 
+/**
+ * \brief BaseSpinLockClass is a class which specify what attribute should
+ * Spinlock have.
+ * \tparam FitIntCacheline is a boolean which represent wheter add padding
+ * to attribute in order fit in cacheline or not.
+ */
 template <bool FitIntCacheline>
 class BaseSpinLockClass {
  protected:
-  std::atomic_flag lock_ = ATOMIC_FLAG_INIT;
+  std::atomic_flag lock_ = ATOMIC_FLAG_INIT; /// underlaying lock.
 };
 
 template <>
@@ -34,6 +43,11 @@ class BaseSpinLockClass<true> {
   char padding[hardware_destructive_interference_size - sizeof(lock_)];
 };
 
+/**
+ * \brief BaseSpinLock is a class which implementation the spinlock.
+ * \tparam FitIntCacheline is a boolean which represent wheter add padding
+ * to attribute in order fit in cacheline or not.
+ */
 template <bool FitIntCacheline>
 class BaseSpinLock : public BaseSpinLockClass<FitIntCacheline> {
  public:
@@ -41,6 +55,9 @@ class BaseSpinLock : public BaseSpinLockClass<FitIntCacheline> {
 
   ~BaseSpinLock() {}
 
+  /**
+   * \brief lock the spinlock.
+   */
   void lock() noexcept {
     while (this->lock_.test_and_set(std::memory_order_acquire)) {
       while (this->lock_.test(std::memory_order_relaxed))
@@ -48,17 +65,27 @@ class BaseSpinLock : public BaseSpinLockClass<FitIntCacheline> {
     }
   }
 
+  /**
+   * \brief unlock the spinlock.
+   */
   void unlock() noexcept { this->lock_.clear(std::memory_order_release); }
 
+  /**
+   * \brief try to lock the spinlock.
+   * \return boolean which tell if locking operation was successful or not.
+   */
   bool try_lock() noexcept {
     return !this->lock_.test_and_set(std::memory_order_acq_rel);
   }
 
+  /**
+   * \brief clear underlaying lock status.
+   */
   void clear() noexcept { this->lock_.clear(std::memory_order_release); }
 };
 
-using SpinLock = BaseSpinLock<false>;
-using CacheLineSpinLock = BaseSpinLock<true>;
+using SpinLock = BaseSpinLock<false>; /// Spinlock does not fit in the cacheline.
+using CacheLineSpinLock = BaseSpinLock<true>; /// SpinLock that fit in the cacheline.
 static_assert(sizeof(CacheLineSpinLock) ==
                   hardware_destructive_interference_size,
               "CacheLine Spinlock not in size cacheline");
